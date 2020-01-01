72ec2146445230dd3745d0ece8b993a8368bad0d
sadorikimen <rikiya0021@gmail.com>
2020-01-01 13:49:00 +0900

    送信機の実装
    送信機能の実装を行いました。
    マスターブランチのまま実装してしまいました。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,9 +24,17 @@ class GroupsController < ApplicationController
     end
   end
 
+  def show
+    @group = Group.find(paramas[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
   private
   def group_params
-    params.require(:group).permit(:name, user_ids: [] )
+    params.require(:group).permit(:name, user_ids: [])
   end
 
   def set_group
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,13 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  has_many :messages
+
+  def show_lastest_message
+    if (lastest_message = messages.last).present?
+      lastest_message.content? ? lastest_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません'
+    end
+  end
 end
