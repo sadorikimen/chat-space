c3191f7d304451919b9ae5083e6802bced99718e
sadorikimen <rikiya0021@gmail.com>
2020-01-18 14:28:24 +0900

    test_code
    テストコードを記述しています。

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -12,14 +12,17 @@ describe MessagesController do
         get :index, params: { group_id: group.id }
       end
       
+      ##
       it 'assigns @message' do
         expect(assigns(:message)).to be_a_new(Message)
       end
 
+      ##
       it 'assigns @group' do
         expect(assigns(:group)).to eq group
       end
 
+      ##
       it 'renders index' do
         expect(response).to render_template :index
       end
@@ -36,4 +39,35 @@ describe MessagesController do
       end
     end
   end
+
+  describe '#create' do
+    let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
+
+    context 'log in' do
+      #この中にログインしている場合のテストをかく
+      before do
+        login user
+      end
+
+      context 'can save' do
+        #この中にメッセージの保存に成功した時のテストをかく
+      end
+  
+      context 'can not save' do
+        #この中にメッセージの保存に失敗した時のテストをかく
+      end
+    end
+
+    context 'not log in' do
+      #この中にログインしていない場合のテストをかく
+      before do
+        get :create, params: params
+      end
+    end
+
+    
+  end
+
+
+
 end
