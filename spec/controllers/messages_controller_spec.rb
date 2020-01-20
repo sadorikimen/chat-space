977343087f5e93d871a4a01fd1cff5b7695ad317
sadorikimen <rikiya0021@gmail.com>
2020-01-20 20:34:42 +0900

    update
    エラーを全て解決しました。

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -12,21 +12,17 @@ describe MessagesController do
         get :index, params: { group_id: group.id }
       end
       
-      ##
       it 'assigns @message' do
         expect(assigns(:message)).to be_a_new(Message)
       end
-
-      ##
+      
       it 'assigns @group' do
         expect(assigns(:group)).to eq group
       end
-
-      ##
+      
       it 'renders index' do
         expect(response).to render_template :index
       end
-
     end
 
     context 'not log in' do
@@ -51,23 +47,51 @@ describe MessagesController do
 
       context 'can save' do
         #この中にメッセージの保存に成功した時のテストをかく
+        subject {
+          post :create,
+          params: params
+        }
+
+        it 'count up message' do
+          expect{ subject }.to change(Message, :count).by(1)
+        end
+
+        it 'redirects to group_messages_path' do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
       end
   
       context 'can not save' do
         #この中にメッセージの保存に失敗した時のテストをかく
+        let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, content: nil, image: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it 'does not count up' do
+          expect{ subject }.not_to change(Message, :count)
+        end
+
+        it 'renders index' do
+          subject
+          expect(response).to render_template :index
+        end
+
+
       end
     end
 
     context 'not log in' do
       #この中にログインしていない場合のテストをかく
-      before do
-        get :create, params: params
+      it 'redirects to new_user_session_path' do
+        post :create, params: params
+        expect(response). to redirect_to(new_user_session_path)
       end
     end
 
-    
+
   end
-
-
-
 end
