8e44dd9a7ff6dd43eadb7204a250463c09e325be
sadorikimen <rikiya0021@gmail.com>
2020-01-28 21:30:46 +0900

    アラートの追記
    アラートの追記をした。
    また、自動更新機能が上手く動いていなかったので、改めて修正しました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -51,7 +51,6 @@ $(function(){
   };
 
   $('#new_message').on('submit', function(e){
-    console.log("OK");
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
@@ -79,24 +78,25 @@ $(function(){
     last_message_id = $('.message:last').data("message-id");
     $.ajax({
       url: "api/messages",
-      type: "get",
+      type: 'get',
       dataType: 'json',
       data: {id: last_message_id}
     })
     .done(function(messages) {
+      console.log(messages)
       if (messages.length !== 0){
         var insertHTML = '';
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
         $('.messages').append(insertHTML);
-        $('.meesages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
         $("#new_message")[0].reset();
         $(".form__submit").prop("disabled", false);
       }
     })
     .fail(function() {
-      console.log('error');
+      alert("メッセージ送信に失敗しました");
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
